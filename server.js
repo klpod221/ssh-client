5f74f3199e038f9e3d91097c6ccad9c8713b26cf
klpod221 <klpod221@gmail.com>
2024-10-28 16:20:54 +0700

    Set views directory for EJS templating in server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const serverPort = 3000;
 
 const server = http.createServer(app);
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
